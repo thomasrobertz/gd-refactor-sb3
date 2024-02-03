735b2acfb369a23814b22cc67181a46236d7e8c2
v134435 <thomas.robertz.extern@generali.de>
2024-02-03 15:24:27 +0100

    initial commit

---
+++ src/main/java/de/robertz/SayHelloRecipe.java
@@ -0,0 +1,35 @@
+package de.robertz;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.Value;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class SayHelloRecipe extends Recipe {
+
+    @Option(displayName = "Fully Qualified Class Name",
+            description = "A fully qualified class name indicating which class to add a hello() method to.",
+            example = "com.yourorg.FooBar")
+    @NonNull
+    String fullyQualifiedClassName;
+
+    @JsonCreator
+    public SayHelloRecipe(@NonNull @JsonProperty("fullyQualifiedClassName") String fullyQualifiedClassName) {
+        this.fullyQualifiedClassName = fullyQualifiedClassName;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "HelloDisplay";
+    }
+
+    @Override
+    public String getDescription() {
+        return "HelloDesc";
+    }
+}
